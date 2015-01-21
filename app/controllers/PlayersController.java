780b9ed44909d40968103355c32645f8959437c8
Marcin Czapla <marcin.czapla@infinite.pl>
2015-01-21 13:05:53 +0100

    dodałem repozytorium graczy

--- app/controllers/PlayersController.java
+++ app/controllers/PlayersController.java
@@ -7,30 +7,36 @@ import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
+import repositories.PlayerRepository;
+import repositories.PlayerRepositorySimpleImpl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Created by Marcin on 2015-01-18.
  */
 public class PlayersController  extends Controller {
 
+    private static PlayerRepository playerRepository = new PlayerRepositorySimpleImpl();
+
     public static Result list(){
-        List<Player> players = new ArrayList<>();
-        players.add(new Player(1L,"Anna"));
+        List<Player> players = playerRepository.getAll();
         return ok(Json.toJson(players));
     }
 
     public static Result show(Long id){
+        Result result =  badRequest("badRequest");
         if(id!=null){
-            Player p = new Player(2L, "John");
-            if(id.equals(2L))
-                return ok(Json.toJson(p));
-            else
-                return notFound("not found");
+            try {
+                Player p = playerRepository.getById(id);
+                result = ok(Json.toJson(p));
+            }catch (NoSuchElementException nsee){
+                result = notFound("not found");
+            }
         }
-        return notFound("nf");
+        return result;
     }
 
     public static Result delete(Long id){
