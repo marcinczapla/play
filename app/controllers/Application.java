1a038b8151a288c8e43265e726731f2927216583
Marcin Czapla <marcin.czapla@infinite.pl>
2015-01-22 09:07:35 +0100

    usuniecie zbednego stringa

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready. mmm"));
+        return ok(index.render("Your new application is ready."));
     }
 
     public static Result defaultControler(String path) {
